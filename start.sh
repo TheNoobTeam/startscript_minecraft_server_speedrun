158d0bae2dc42a0b0f2e296c92726a551953a0be
doman991 <d.kubiak196@gmail.com>
2021-02-04 10:25:13 +0000

    start.sh
    This start script is for co-op minecraft speedruners who are not able to use any external modifications / plugins etc.
    This start scipt:
    - Removes your all already existing wolrds
    - Start the server imidenetly after previous worlds are gone
    - Start the server when crash (and remove all existing worlds)
    - Removes main world, nether and the end

---
+++ start.sh
@@ -0,0 +1,19 @@
+#Path to your minecraft server location.
+cd /home/debian/speedrun
+for (( ; ; ))
+do
+#Removing all already existing worlds. If you changed default level-name in server.properties just replace "world" with your custom level-name and remember to keep _nether and _the_end.
+   echo removing worlds...
+   rm -r world
+   rm -r world_nether
+   rm -r world_the_end
+
+   echo worlds removed.
+   echo starting Minecraft server.
+
+#Here you can change your minimum and maximum.(replacing the jar name by whatever you named the jar file).
+   java -Xms1G -Xmx3G -jar Minecraft.jar
+   
+   sleep 10
+   echo resuming server...
+done
